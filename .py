f7021ef4c25cf2899e130787603d8c1cb4ff24fe
Haryu5412 <haryu5412@gbsw.gyo6.net>
2025-06-12 08:36:06 +0900

    진행도 리셋, main.py에다가 모델 테스트

--- .py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyA1ZkSTHki91LwNyB5623ik9bbSVGU6ODE")
-models = genai.list_models()
-
-for m in models:
-    print(m.name, "-", m.supported_generation_methods)
