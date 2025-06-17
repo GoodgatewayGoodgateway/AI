c11af9e05d6a6932b6ce52af87a8c20be87e9a2a
Haryu5412 <haryu5412@gbsw.gyo6.net>
2025-06-17 09:25:25 +0900

    포트랑 아이피 .env에서 지정하게 경로 변경

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
 from app.routes import housing_detail
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # .env 파일 로드
 
 app = FastAPI()
 
 app.include_router(housing_detail.router, prefix="/api", tags=["Housing Detail"])
+
+if __name__ == "__main__":
+    import uvicorn
+
+    host = os.getenv("HOST")
+    port = int(os.getenv("PORT"))
+
+    uvicorn.run("main:app", host=host, port=port, reload=True)
